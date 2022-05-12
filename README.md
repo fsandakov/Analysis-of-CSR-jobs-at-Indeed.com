55baf00e2ce64a9f725e79ec54c4b74f17246be2
fsandakov <98375260+fsandakov@users.noreply.github.com>
2022-05-12 06:56:08 -0400

    done

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# Analysis of CSR jobs at Indeed.com
- Analysis of CSR jobs at Indeed.com
+# CSR jobs at Indeed.com
+CSR jobs at Indeed.com
+
+
+Purpose of the analysis: 
+The original purpose of the project was to analyze the offer of jobs from the corporate and social responsibility jobs at Indeed.com. During the project, the inconsistencies at the website's code (such as, for example, with salary posting) and the complexity of parsing data in general as oposed to using API, the purpose of the project shifted to creating a script that could help people make databases with information on available postings in their field of interest so that they would understand what really is on offer without having to go through the tiresome process of going through the search results and different queries. Using the result database, a person can see the information in a structured way and filter the positions in the way they want.
+
+Input data:
+The input data is the list of queries that is required from user to collect hob postings of interest.
+
+Output:
+The output is an Excel file "jobs.xsls" with field-specific current job postings database including 17 variables: title, location, city, state, remote (are remote/hybrid modes available), jobtype (full-time, part-time, contract), organization, orglink, rating (of the organization), rating_count (number of people who voted), salary_text (what was written in the according fields), salary (min and max if available), salary_av (average), description, posted (how many days ago was the job posted), joblink.
+
+Script functions:
+The script 'parsingdata.py' serves to parse the job offerings data from Indeed.com. The script is has a lot of comments, so it that it would be easy to use and adapt if needed.
+The main parts of the code include:
+1) Creation of links based on user's queries.
+2) Parsing of the list of links to the search pages.
+3) Parsing of the links to the job pages from the search results pages.
+4) Parsing of data from the job posting pages and the creation of a database.
+5) Quick analysis of the received data (offer of jobs by organization, city, and state).
+
+Results:
+The updated project idea was successfully implemented, and a database of 719 job postings in the area of CSR was created, among other datasets.
+I find some of my coding solutions fantastic, as sometimes after several mig solutions I would find replacements for them that would only take one line of code and make everything pretty and concise. I used a lot of regex and became profficient in it, along with parsing. Idea-wise, I think my solution with putting all positions form different searches in one database is great, as sometimes searches overlap by up to 700 job postings, and time is too valuable to waste on looking at positions one has already considered.
+The main problems included inconsistency in the way Indeed.com is coded, which caused a lot of errors and script adaptation on my side, and the fact that after several parsing efforts I had to start using a new VPN connection for each 100 of search results, as the searches would start to show void due to Indeed.com protection system.
+As for results of my quick analysis, look below.
+Counts by organization:
+
+                            organization  counts
+    257                   Neiman Marcus      41
+    200       JPMorgan Chase Bank, N.A.      21
+    199                             JLL      21
+    20          Amazon.com Services LLC      14
+    206                            KPMG      10
+    ..                              ...     ...
+    164                        Goby Inc       1
+    163           Geosyntec Consultants       1
+    162                Genomatica, Inc.       1
+    161                 Genomatica, Inc       1
+    430  thyssenkrupp Materials NA Inc.       1
+    
+So, there are 431 organizations in my dataset, and Neiman Marcus is the one that is developing the most actively in the field in terms of hiring new people at the moment. Most of the top organizations here belong to big tech of are financial organizations.
+
+
+
+By city comparisson:
+
+              city  counts
+84        New York      71
+141     Washington      25
+117  San Francisco      15
+24         Chicago      14
+..             ...     ...
+1          Allegan       1
+34   Daytona Beach       1
+72        Melville       1
+71          McLean       1
+147         Woburn       1
+
+There are 148 cities in the search results, and NYC and Washington are expectedly occupying the first two rows. The next two most popular cities right now are San Francisco, and Chicago, which I did not initially expect, as I was not looking for jobs in Chicago before.
+
+
+
+By state comparisson:
+
+23    NY      82
+3     CA      56
+6     DC      25
+10    IL      23
+14    MA      18
+33    WA      15
+30    TX      15
+
+Apparently, there are a lot of jobs in California in places other  than San Francisco, as the state climbed on the second spot in the by state summary table.
--- parsingdata.py
+++ parsingdata.py
@@ -2,7 +2,6 @@
 
 import requests
 from bs4 import BeautifulSoup
-import csv
 import re
 import pandas as pd
 
@@ -17,10 +16,13 @@ def make_url(position, location=''):
     return URL
 
 # give query list and build search links based on queries
-querylist=[('corporate responsibility', 'New York, NY')
-            ,('corporate responsibility','')
-            ,('corporate social responsibility','')
-            ,('CSR','')]
+querylist=[('corporate responsibility','')
+           ,('corporate social responsibility','')
+           ,('corporate philantropy','')
+           ,('ESG','')]
+           #add location in the second quotation marks if you have preference:
+           #,('corporate responsibility', 'New York, NY')
+           
 URLS=[]
 for position, location in querylist:
     URL = make_url(position, location)
@@ -28,7 +30,7 @@ for position, location in querylist:
     
 print('The list of initial search links:\n', URLS)
 
-# create the list of search page links for the search
+# create the list of links to the search pages
 all_search_pages=[]
 for URL in URLS:
     nextpage = URL
@@ -61,7 +63,8 @@ for url in all_search_pages:
         links.append(alink.get('href'))
     
     #create a list of relevant job pages
-    pages=["https://www.indeed.com"+x for x in links if x is not None and ('/pagead/' in x or '/rc/' in x)]
+    # if you want to include ads change the last part to ('/pagead/' in x or '/rc/' in x)
+    pages=["https://www.indeed.com"+x for x in links if x is not None and '/rc/' in x]
     jobpages = jobpages + pages
     
 
@@ -71,15 +74,18 @@ print('Number of relevant job postings: \n', len(jobpages))
 jobpages = list(set(jobpages))
 print('Number of relevant job postings without duplicates: \n', len(jobpages))
     
-print(jobpages)
+# print(jobpages)
 
 # Now here is the part that extracts all the data from the job pages
+# needed variablees
 title = ''
 organization = ''
 orglink = ''
 var = ''
 remote=''
 location=''
+city = ''
+state = ''
 rating=None
 rating_count=None
 salary_text=''
@@ -91,38 +97,59 @@ description=''
 posted=''
 joblink = ''
 #the df that we are going to fill
-df = pd.DataFrame(columns=['title', 'location', 'remote', 'jobtype', 'organization'
-                           , 'orglink', 'rating', 'rating_count', 'salary_text'#, 'salary','salary_av'
+df = pd.DataFrame(columns=['title', 'location', 'city', 'state', 'remote', 'jobtype', 'organization'
+                           , 'orglink', 'rating', 'rating_count', 'salary_text', 'salary','salary_av'
                            , 'description', 'posted', 'joblink'])   
-
-salary_pattern = re.compile(r'\d+(\,?\d+)*(\.?\d+)*') 
-#the pattern above has an error, so I'm trying to find the correct one
+    
+salary_pattern = re.compile(r'[0-9]+(?:[,][0-9]+)*[.]?[0-9]*') 
 num_pattern = re.compile(r'\d+') 
 stars_pattern = re.compile(r"\d+\.?\d*")
-permonth_pattern = re.compile(r'month')
-perhour_pattern = re.compile(r'hour')
-salaryKpattern = re.compile(r'(\d+,?\d+)+\.?\d*K')
+salaryKpattern = re.compile(r'(?:[0-9]+(?:[,][0-9]+)*[.]?[0-9]*[Kk])')
+extrasymbolspattern = re.compile(r'((?<=-\s\s))?.*')
+city_pattern = re.compile(r'.*(?=,)')
+state_pattern = re.compile(r'(?<=,)\s+[A-Z]+')
+not_parsed_jobs=[]
 
-#change the brackets to get all links' data parsed
-for url in jobpages[:10]:
+for url in jobpages:
     
     joblink = url
     
     html = requests.get(url)
     soup = BeautifulSoup(html.text, 'html.parser')
     
-    title = soup.find(attrs = {'class':'icl-u-xs-mb--xs icl-u-xs-mt--none jobsearch-JobInfoHeader-title'}).text
-    
-    var = soup.find(attrs = {'class':'icl-u-xs-mt--xs icl-u-textColor--secondary jobsearch-JobInfoHeader-subtitle jobsearch-DesktopStickyContainer-subtitle'})
-    varloc = var.find_all('div', attrs = {'class':''})
-    remote = varloc[-1].text
-    location = varloc[-3].text
-
     try:
-        jobtype = soup.find(attrs = {'class':'jobsearch-JobMetadataHeader-item  icl-u-xs-mt--xs'}).text
+        title = soup.find(attrs = {'class':'icl-u-xs-mb--xs icl-u-xs-mt--none jobsearch-JobInfoHeader-title'}).text
     except:
-        jobtype = ''
+        # there are links to other websites sometimes, and this code in only suitable for indeed.com
+        not_parsed_jobs.append(url)
+        continue 
+    
+    try:
+        var = soup.find(attrs = {'class':'icl-u-xs-mt--xs icl-u-textColor--secondary jobsearch-JobInfoHeader-subtitle jobsearch-DesktopStickyContainer-subtitle'})
+        varloc = var.find_all('div', attrs = {'class':''})
+        remote = varloc[-1].text
+        location = varloc[-3].text
+    except:
+        remote = ''
+        location = ''
+    
+    try:
+        city = city_pattern.search(location).group()
+        state = state_pattern.search(location).group()
+    except:
+        city = None
+        state = None
         
+    if location == "Remote":
+        remote = location
+    
+    # job type    
+    if soup.find('span',attrs={'class':'jobsearch-JobMetadataHeader-item icl-u-xs-mt--xs'})!=None:
+        jobtype = soup.find('span',attrs={'class':'jobsearch-JobMetadataHeader-item icl-u-xs-mt--xs'}).text
+        if "-  " in jobtype:
+            jobtype = re.findall(extrasymbolspattern,jobtype)[0]
+
+    
     varorg = var.find_all(attrs = {'class':'icl-u-lg-mr--sm icl-u-xs-mr--xs'})
     organization = varorg[-1].text
     try:
@@ -132,53 +159,57 @@ for url in jobpages[:10]:
 
     #rating
     try:
-        rating = "{:.2f}".format(float(stars_pattern.findall(soup.find(attrs={'class':'icl-Ratings-starsFilled'}).get('style'))[0]) / 9) #divide to get out of 10 rating
-        rating_count = int(num_pattern.findall(soup.find(attrs={'class':'icl-Ratings-count'}).text.replace(',',''))[0])
+        rating = float(soup.find('meta', attrs={'itemprop':'ratingValue'}).get('content'))
+        rating_count = int(soup.find('meta', attrs={'itemprop':'ratingCount'}).get('content'))
     except:
         rating = None
         rating_count = None
     
     #salary
-    try:
+    if soup.find(attrs = {'class':'icl-u-xs-mr--xs attribute_snippet'})!=None:
         salary_text = soup.find(attrs = {'class':'icl-u-xs-mr--xs attribute_snippet'}).text
         
         ##### The rest of the salary part does not work as planned,
         ##### so I leave the text field as a variable in the df
         salary = re.findall(salary_pattern, salary_text)
-        for i in salary:
-            i=i.replace(',','')
-        # print(salary)
-        # if salary is in thousands
-        if re.findall(salaryKpattern, salary_text)!=None:
-            for i in salary:
-                salary[i] = float(salary[i])*1000
+        #get rid of commas
+        salary = [float(i.replace(',','')) for i in salary]
+
         # if salary is per month
-        if re.findall(permonth_pattern, salary_text)!=None:
-            for i in salary:
-                salary[i] = float(salary[i])*12
-        #if hourly wage given (assumption: 40 hrs per wek)
-        if re.findall(perhour_pattern, salary_text)!=None:
-            for i in salary:
-                salary[i] = float(salary[i])*40*52
-                
+        if 'month' in salary_text:
+            salary = [int(i * 12) for i in salary]
+        #if salary is given per week (assumption: 52 weeks)
+        if 'week' in salary_text:
+            salary = [int(i * 52) for i in salary]
+        #if hourly wage given (assumption: 40 hrs per week)
+        if 'hour' in salary_text:
+            salary= [int(i *40 *52) for i in salary]
+                        
         # if salary is not given and there is a salary guide
-        if soup.find(attrs={'id':'salaryGuide'})!=None:
-            salary_text = soup.find(attrs={'id':'salaryGuide'}).text
-            salary = re.findall(salary_pattern, salary_text)
-            for i in salary:
-                i=i.replace(',','')
-            if re.findall(salaryKpattern, salary_text)!=None:
-                for i in salary:
-                    salary[i] = int(float(salary[i])*1000)
+    elif soup.find(attrs={'id':'salaryGuide'})!=None:
+        salary_text = soup.find(attrs={'id':'salaryGuide'}).text
+        salary = re.findall(salary_pattern, salary_text)
+        salary = [float(i.replace(',','')) for i in salary]
         
-        # create a variable with average salary        
+        # salary is given in thousands
+        if re.findall(salaryKpattern, salary_text)!=None:
+            salary = [int(i * 1000) for i in salary]
+
+     
+        
+    # create a variable with average salary        
+    try:
         salary_av = sum(salary)/len(salary)
     except:
-        salary=[]
-        salary_av = None
-        
+        salary_av=None
+
+    
     # job description    
-    description = soup.find(attrs = {'id':'jobDescriptionText'}).text.strip()
+    try:
+        description = soup.find(attrs = {'id':'jobDescriptionText'}).text.strip()
+    except:
+        description = ''
+    
     
     ##### the list of benefits
     ##### this part would work but the website updates several seconds after 
@@ -190,42 +221,99 @@ for url in jobpages[:10]:
     #         benefits[i]=benefits[i].text
     # except:
     #     benefits=[]
-       
+    
+    
+    # How many days ago the job posting was made:
     var = soup.find(attrs = {'class':'jobsearch-JobMetadataFooter'})
     varloc = var.find_all('div', attrs = {'class':''})
-    posted = num_pattern.findall(varloc[0].text)[0]
     
-    
+    if "Just" in varloc[0].text or "Today" in varloc[0].text:
+        posted = 0
+    elif "30+" in varloc[0].text:
+        posted = 31 # = 31 if more than 30 days
+    else:
+        posted = num_pattern.findall(varloc[0].text)[0]
+
     # create a df with data on the new job
-    appended_df = pd.DataFrame([[title, location, remote, jobtype, organization
-                               , orglink, rating, rating_count, salary_text#, salary, salary_av
+    appended_df = pd.DataFrame([[title, location, city, state, remote, jobtype, organization
+                               , orglink, rating, rating_count, salary_text, salary, salary_av
                                , description, posted, joblink]], 
-                               columns=['title', 'location', 'remote', 'jobtype', 'organization'
-                                        , 'orglink', 'rating', 'rating_count', 'salary_text'#, 'salary','salary_av'
+                               columns=['title', 'location',  'city', 'state', 'remote', 'jobtype', 'organization'
+                                        , 'orglink', 'rating', 'rating_count', 'salary_text', 'salary','salary_av'
                                         , 'description', 'posted', 'joblink'])     
     
     # add data to the main df
-    df = pd.concat([appended_df, df], axis=0, ignore_index=True)                 
+    df = pd.concat([appended_df, df], axis=0, ignore_index=True) 
+    
+    salary_text = None
+    salary=[]
+    salary_av = None
+    city = ''
+    state = ''
+
+print("The number of jobs that were not parsed because the link led to a different website:", len(not_parsed_jobs))
+          
 df.head()
-df.to_csv('jobs.csv', index=False, encoding='utf-8')
-
-    ## Some Analysis
-#biggest provider
-df.groupby(['organization']).size()
-#most highly rated org
-df.sort_values(by=['rating'])
+df.to_excel("jobs.xlsx")
 
     
     
+# Some Analysis
+
+byorg=df.groupby(['organization']).size().reset_index(name='counts')
+byorg.sort_values(['counts'],ascending=False).groupby('organization').head()
+
+#                             organization  counts
+#     257                   Neiman Marcus      41
+#     200       JPMorgan Chase Bank, N.A.      21
+#     199                             JLL      21
+#     20          Amazon.com Services LLC      14
+#     206                            KPMG      10
+#     ..                              ...     ...
+#     164                        Goby Inc       1
+#     163           Geosyntec Consultants       1
+#     162                Genomatica, Inc.       1
+#     161                 Genomatica, Inc       1
+#     430  thyssenkrupp Materials NA Inc.       1
     
+# So, there are 431 organizations in my dataset, and Neiman Marcus is the one 
+# that is developing the most actively in the field in terms of hiring new people at the moment.
+# Most of the top organizations here belong to big tech of are financial organizations.
+
+bycity=df.groupby(['city']).size().reset_index(name='counts')
+bycity.sort_values(['counts'],ascending=False).groupby('city').head()
+
+#               city  counts
+# 84        New York      71
+# 141     Washington      25
+# 117  San Francisco      15
+# 24         Chicago      14
+# ..             ...     ...
+# 1          Allegan       1
+# 34   Daytona Beach       1
+# 72        Melville       1
+# 71          McLean       1
+# 147         Woburn       1
+
+# So, there are 148 cities in the search results, and NYC and Washington are 
+# expectedly occupying the first two rows. The next two most popular cities right now
+# are San Francisco, and Chicago, which I did not initially expect, as I was not 
+# looking for jobs in Chicago before.
+
+
+bystate=df.groupby(['state']).size().reset_index(name='counts')
+bystate.sort_values(['counts'],ascending=False).groupby('state').head()
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
+# 23    NY      82
+# 3     CA      56
+# 6     DC      25
+# 10    IL      23
+# 14    MA      18
+# 33    WA      15
+# 30    TX      15
+
+# Apparently, there are a lot of jobs in California in places other  than San Francisco,
+# as the state climbed on the second spot in the by state summary table.
+
+
     
